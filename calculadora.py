46cecf20b95f19b9bafd13b5dfb334e9ff3f6a4d
Orlando-Oyardo <orlando.oyardo@gmail.com>
2024-11-06 10:38:26 -0500

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -3,18 +3,9 @@ def suma(a, b):
 
 def resta(a, b):
     return a - b
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
-=======
 
 def multiplicacion(a, b):
     return a * b
->>>>>>> multiplicacion-feature
-=======
-=======
->>>>>>> potencia-feature
 
 def multiplicacion(a, b):
     return a * b
@@ -23,10 +14,6 @@ def division(a, b):
     if b == 0:
         return "Error: División por cero."
     return a / b
-<<<<<<< HEAD
->>>>>>> division-feature
-=======
 
 def potencia(a, b):
     return a ** b
->>>>>>> potencia-feature
