2275e32f44b127286286346d5efcc97f55ae6155
Orlando Oyardo <orlando.oyardo@gmail.com>
2024-11-05 23:02:43 -0500

    funcion potencia

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        return "Error: División por cero."
+    return a / b
+
+def potencia(a, b):
+    return a ** b
